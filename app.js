5f223dd2f62bf52e3d1463e04b91611474c2fb7a
tej <tejasreed73@gmail.com>
2026-04-01 22:01:50 +0530

    extra files

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use("/api/queues", queueRoutes);
 const adminRoutes = require("./Backend/routes/adminRoutes");
 app.use("/api/admin", adminRoutes);
 
+const paymentRoutes = require("./Backend/routes/paymentRoutes");
+app.use("/api/payment", paymentRoutes);
+
 const { protect, adminOnly } = require("./middleware/authMiddleware");
 
 // Protected route
